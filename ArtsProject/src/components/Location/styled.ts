9be369e6b065e8a1c06fb6efb907f5117009a7c6
songyiiii <gksthddl43@daum.net>
2024-12-03 15:19:42 +0900

    회사소개 수정

--- ArtsProject/src/components/Location/styled.ts
+++ ArtsProject/src/components/Location/styled.ts
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 export const LocationStyled = styled.div`
-    width: 100%;
-    height: 400px;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    background-color: #f7f7f7;
-    @media screen and (max-width: 480px) {
-        flex-direction: column;
-}
-@media screen and (min-width: 481px) and (max-width: 768px) {
-   flex-direction: column;
-   align-items: start;
-}
-
+  width: 100%;
+  height: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  background-color: #f7f7f7;
+  @media screen and (max-width: 480px) {
+    flex-direction: column;
+    align-items: start;
+  }
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+    flex-direction: column;
+    align-items: start;
+  }
 `;
--- ArtsProject/src/components/Map/styled.ts
+++ ArtsProject/src/components/Map/styled.ts
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
 
 export const MapStyled = styled.div`
-    width: 40%;
-    height: 80%;
-    border-radius: 15px;
-    background-color: green;
-    margin: auto;
-    @media screen and (max-width: 480px) {
-        width: 60%;
-        margin-bottom: 10px;
-    }
-    @media screen and (min-width: 481px) and (max-width: 768px) {
-        width: 60%;
-        margin-bottom: 20px;
-    }
+  width: 50%;
+  height: 80%;
+  border-radius: 15px;
+  background-color: green;
+  margin-right: 20px;
+  /* margin: auto; */
+  @media screen and (max-width: 480px) {
+    width: 100%;
+    margin-bottom: 10px;
+  }
+  @media screen and (min-width: 481px) and (max-width: 768px) {
+    width: 100%;
+    margin-bottom: 20px;
+  }
 `;
